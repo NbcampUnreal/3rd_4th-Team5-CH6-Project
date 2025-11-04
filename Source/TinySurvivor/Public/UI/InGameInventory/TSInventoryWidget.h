3231f493335bac546c8a22d3f60e76a575990d2d
seowooyoung <seohascome20th@naver.com>
2025-11-04 12:50:46 +0900

    UI 기초 세팅
    CommonUI 를 활용하여 인 게임, 인벤토리 UI 구현 (현재 임시로 플레이어 컨트롤러에 있으며, 추후 HUD로 옮길 예정, 인벤토리 토글 키 I 또는 T 입니다. DebugKey 로 해둠.)
    
    인벤토리 드래그 앤 드랍 기초 구현 -> 이미지 드래그 테스트 확인
    
    UI컨트롤러 매니저 시스템 구현 연동 테스트 확인 (현재는 다시 끊어놨음)
    
    # Conflicts:
    #	Content/Blueprint/BP_TSGameMode.uasset

---
+++ Source/TinySurvivor/Public/UI/InGameInventory/TSInventoryWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "TSInventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYSURVIVOR_API UTSInventoryWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+};
