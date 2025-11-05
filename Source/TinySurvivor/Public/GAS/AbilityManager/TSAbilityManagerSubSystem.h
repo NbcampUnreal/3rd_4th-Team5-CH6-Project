a6dd0abda630aa82d6af745f68ea4e1da9794491
seop0102 <seop0102@kookmin.ac.kr>
2025-11-05 21:23:27 +0900

    feat(Character) : 어빌리티 매니저 생성

---
+++ Source/TinySurvivor/Public/GAS/AbilityManager/TSAbilityManagerSubSystem.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "TSAbilityManagerSubSystem.generated.h"
+
+struct FGameplayTag;
+class UAbilitySystemComponent;
+class UDataTable;
+
+UCLASS()
+class TINYSURVIVOR_API UTSAbilityManagerSubSystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	void GiveAbilityByTag(UAbilitySystemComponent* ASC, FGameplayTag DesiredTag, FName TableKey = "Default", int32 AbilityLevel = 1, int32 InputID = -1) const;
+	void ActivateAbilityByTag(UAbilitySystemComponent* ASC, FGameplayTag DesiredTag, FName TableKey = "Default", int32 AbilityLevel = 1, int32 InputID = -1) const;
+	void ClearAbilityByTag(UAbilitySystemComponent* ASC, FGameplayTag DesiredTag, FName TableKey = "Default", int32 AbilityLevel = 1, int32 InputID = -1) const;
+
+
+	static UTSAbilityManagerSubSystem* GetAbilityManager(const UObject* WorldContextObject)
+	{
+		if (!WorldContextObject) return nullptr;
+
+		const UGameInstance* GameInstance = UGameplayStatics::GetGameInstance(WorldContextObject);
+		if (!GameInstance) return nullptr;
+
+		return GameInstance->GetSubsystem<UTSAbilityManagerSubSystem>();
+	}
+
+protected:
+	UPROPERTY()
+	TMap<FName, UDataTable*> AbilityTables;
+};
