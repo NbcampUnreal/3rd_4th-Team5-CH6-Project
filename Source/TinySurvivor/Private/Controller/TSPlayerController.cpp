04ae5826c1d3c5c213fa09ee28f126e90c0023c7
seop0102 <seop0102@kookmin.ac.kr>
2025-11-07 21:31:13 +0900

    feat(Controller) : 플레이어 HUD 초기화 함수 틀 생성
    InitializePlayerHUD 틀 구현

--- Source/TinySurvivor/Private/Controller/TSPlayerController.cpp
+++ Source/TinySurvivor/Private/Controller/TSPlayerController.cpp
@@ -1,2 +1,9 @@
 #include "Controller/TSPlayerController.h"
+#include "Character/TSCharacter.h"
+
+void ATSPlayerController::InitializePlayerHUD(ATSCharacter* InCharacter)
+{
+	UE_LOG(LogTemp, Warning, TEXT("InitializePlayerHUD calling"));
+	//나중에 이 함수 구현해주세여
+}
 
--- Source/TinySurvivor/Public/Controller/TSPlayerController.h
+++ Source/TinySurvivor/Public/Controller/TSPlayerController.h
@@ -4,10 +4,14 @@
 #include "GameFramework/PlayerController.h"
 #include "TSPlayerController.generated.h"
 
+class ATSCharacter;
 
 UCLASS()
 class TINYSURVIVOR_API ATSPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void InitializePlayerHUD(ATSCharacter* InCharacter);
 };
