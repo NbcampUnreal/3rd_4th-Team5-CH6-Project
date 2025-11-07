7922bdd45bc490578954c68b317e7243f266e5be
seop0102 <seop0102@kookmin.ac.kr>
2025-11-07 21:33:26 +0900

    feat(Character) : 핫키 입력 GameplayEvent 연동
    캐릭터에 SendHotKeyEvent 구현 및 Input.HotKey 이벤트 태그 발송
    0~9 핫키 입력시 슬롯 인덱스를 이벤트 페이로드로 전달
    BeginPlay에서 InitializePlayerHUD 호출 연동

--- Source/TinySurvivor/Public/Character/TSCharacter.h
+++ Source/TinySurvivor/Public/Character/TSCharacter.h
@@ -28,12 +28,10 @@ public:
 	
 	virtual void PossessedBy(AController* NewController) override; //서버빙의시호출
 	virtual void OnRep_PlayerState() override; //클라 복제시 호출
-	
-	
-	FORCEINLINE USpringArmComponent* GetSpringArmComponent() const { return SpringArmComponent; }
-	FORCEINLINE UCameraComponent* GetCameraComponent() const { return CameraComponent; }
 
 #pragma region Camera
+	FORCEINLINE USpringArmComponent* GetSpringArmComponent() const { return SpringArmComponent; }
+	FORCEINLINE UCameraComponent* GetCameraComponent() const { return CameraComponent; }
 	// 카메라 
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = "Camera" , meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class USpringArmComponent> SpringArmComponent;
@@ -61,6 +59,7 @@ private:
 protected:
 	virtual void BeginPlay() override;
 
+#pragma region Function
 	// GAS 안쓰는 함수
 	void Move(const struct FInputActionValue& Value);
 	void Look(const struct FInputActionValue& Value);
@@ -73,18 +72,15 @@ protected:
 	void OnSprintStarted(const struct FInputActionValue& Value);
 	void OnSprintCompleted(const struct FInputActionValue& Value);
 	void OnLyingDown(const struct FInputActionValue& Value);
-
-
 	
-	
-	 
 	// Interaction 함수들
 	void OnBuild(const struct FInputActionValue& Value);
 	void OnInteract(const struct FInputActionValue& Value);
 	void OnLeftClick(const struct FInputActionValue& Value); //얘넨 모르겠다 (한 키에 여러가지 함수?)
 	void OnRightClick(const struct FInputActionValue& Value); // 얘넨 모르겠다.
 	void OnPing(const struct FInputActionValue& Value);
-
+	void OnWheelScroll(const struct FInputActionValue& Vaule);
+	
 	//hot key 함수들
 	void OnHotKey1(const struct FInputActionValue& Value);
 	void OnHotKey2(const struct FInputActionValue& Value);
@@ -96,8 +92,11 @@ protected:
 	void OnHotKey8(const struct FInputActionValue& Value);
 	void OnHotKey9(const struct FInputActionValue& Value);
 	void OnHotKey0(const struct FInputActionValue& Value);
-
 	
+#pragma endregion
+	
+private:
+	void SendHotKeyEvent(int HotKeyIndex);
 	
 public:	
 	virtual void Tick(float DeltaTime) override;
