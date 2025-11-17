add54704a6c923bdd7be8679d0869fde17cf49cd
seop0102 <seop0102@kookmin.ac.kr>
2025-11-17 21:04:56 +0900

    feat(Character) : GA Base 클래스 생성
    틀만 잡아둠. 아직 미완성

---
+++ Source/TinySurvivor/Public/BaseAbility/TSGameplayAbilityBase.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "TSGameplayAbilityBase.generated.h"
+
+UCLASS()
+class TINYSURVIVOR_API UTSGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+public: 
+	UTSGameplayAbilityBase();
+	
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+	
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly , Category = "Montage")
+	TObjectPtr<UAnimMontage> FirstMontage;
+	 
+	// 몽타주가 정상적으로 재생을 시작했을 때 호출
+	UFUNCTION()
+	virtual void OnMontageStarted();
+	// 몽타주가 재생 끝나고 블렌드 아웃이 시작될 때 호출
+	UFUNCTION()
+	virtual void OnMontageBlendOut();
+	// 중간에 끊겼을 때
+	UFUNCTION()
+	virtual void OnMontageInterrupted();
+	// 어빌리티 자체가 cancel 되었을 때 호출
+	UFUNCTION()
+	virtual void OnMontageCancelled();
+	// 몽타주가 끝까지 재생을 다 마쳤을 때 호출
+	UFUNCTION()
+	virtual void OnMontageCompleted();
+};
