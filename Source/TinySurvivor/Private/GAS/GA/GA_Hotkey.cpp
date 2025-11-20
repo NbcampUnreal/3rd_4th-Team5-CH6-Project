346aa62cdcf43c5f6a9e5b5e98b4b6d35268c9a4
SKim041 <sehee1806@gmail.com>
2025-11-20 19:32:20 +0900

    feat(GAS): GA_Hotkey 구현
    AbilitySystem을 통해 핫키 어빌리티가 자동으로 활성화되도록 처리.
    - `UGA_Hotkey` 클래스 생성 및 서버 실행 정책(ServerOnly) 설정
    - 어빌리티 부여시 자동 활성화 로직 구현

---
+++ Source/TinySurvivor/Private/GAS/GA/GA_Hotkey.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GA/GA_Hotkey.h"
+
+#include "AbilitySystemComponent.h"
+
+UGA_Hotkey::UGA_Hotkey()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::ServerOnly;
+}
+
+void UGA_Hotkey::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	if (!ActorInfo)
+	{
+		return;
+	}
+
+	if (ActorInfo->IsNetAuthority() && !Spec.IsActive())
+	{
+		UAbilitySystemComponent* ASC = ActorInfo->AbilitySystemComponent.Get();
+		if (ASC)
+		{
+			ASC->TryActivateAbility(Spec.Handle);
+		}
+	}
+}
