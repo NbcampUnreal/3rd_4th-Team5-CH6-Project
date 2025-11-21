849d57b45710e1b93deb898fa8da08e1286b21ae
seop0102 <seop0102@kookmin.ac.kr>
2025-11-21 12:45:42 +0900

    feat(TAG) : AbilityGameplayTags 파일 구현
    NativeTag 사용 후 ini 에 있는 태그들 제거

---
+++ Source/TinySurvivor/Private/GameplayTags/AbilityGameplayTags.cpp
@@ -0,0 +1,35 @@
+// AbilityGameplayTags.cpp
+#include "GameplayTags/AbilityGameplayTags.h"
+
+namespace AbilityTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Move_JumpOrClimb, "Ability.Move.JumpOrClimb");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Move_Roll, "Ability.Move.Roll");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Move_Sprint, "Ability.Move.Sprint");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Move_Crouch, "Ability.Move.Crouch");
+
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact_Build, "Ability.Interact.Build");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact_Interact, "Ability.Interact.Interact");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact_Ping, "Ability.Interact.Ping");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact_LeftClick, "Ability.Interact.LeftClick");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact_RightClick, "Ability.Interact.RightClick");
+
+	
+	
+	
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_HotKey, "Ability.HotKey");
+	
+	
+	
+	
+	UE_DEFINE_GAMEPLAY_TAG(TAG_State_Move_Sprint, "State.Move.Sprint");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_State_Move_Crouch, "State.Move.Crouch");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_State_Move_Roll, "State.Move.Roll");
+	
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Input_HotKey, "Input.HotKey");
+
+	
+	
+	
+	UE_DEFINE_GAMEPLAY_TAG(TAG_State_Status_StaminaPaused, "State.Status.StaminaPaused");
+}
