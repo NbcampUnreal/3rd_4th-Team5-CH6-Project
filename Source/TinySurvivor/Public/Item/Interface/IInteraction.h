2b764d5b013237264db352d9a2f9b171307199e8
SKim041 <sehee1806@gmail.com>
2025-11-21 21:08:38 +0900

    feat(Item): 상호작용 시스템 구현 및 인터랙션 기반 클래스 추가
    아이템 상호작용을 처리하기 위한 인터페이스 및 상호작용 관련 클래스와 로직 추가.
    - `IInteraction`: 상호작용 인터페이스 선언, 상호작용 위젯 표시/숨김, 상호작용 가능 여부 등 메서드 정의
    - `TSInteractionActorBase`: 상호작용을 담당하는 기반 클래스. 상호작용 위젯 초기화 및 데이터 동기화 처리 구현
    - `ATSContainer`: 컨테이너 클래스 추가. 상호작용 및 상자/창고 접근 처리
    - `ATSPickupItem`: 아이템 획득 기능 구현. 상호작용 시 플레이어 인벤토리에 아이템 추가
    - `TSContainer.h` 및 `TSPickupItem.h`: 상호작용 메서드와 데이터 구조 선언
    
    상호작용 시스템을 통해 플레이어와 월드 간의 아이템 상호작용 기반을 마련.

---
+++ Source/TinySurvivor/Public/Item/Interface/IInteraction.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IInteraction.generated.h"
+
+class UWidgetComponent;
+class ATSCharacter;
+
+UINTERFACE()
+class UIInteraction : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class TINYSURVIVOR_API IIInteraction
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ShowInteractionWidget(ATSCharacter* InstigatorCharacter) = 0;
+	virtual void HideInteractionWidget() = 0;
+	virtual void SetInteractionText(FText InteractionText) = 0;
+	virtual bool CanInteract(ATSCharacter* InstigatorCharacter) = 0;
+	virtual void Interact(ATSCharacter* InstigatorCharacter) = 0;
+	virtual bool RunOnServer() = 0;
+};
---
+++ Source/TinySurvivor/Public/Item/TSContainer.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TSInteractionActorBase.h"
+#include "TSContainer.generated.h"
+
+class UTSInventoryMasterComponent;
+
+UCLASS()
+class TINYSURVIVOR_API ATSContainer : public ATSInteractionActorBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATSContainer();
+#pragma region IInteraction
+	virtual bool CanInteract(ATSCharacter* InstigatorCharacter) override;
+	virtual void Interact(ATSCharacter* InstigatorCharacter) override;
+	virtual bool RunOnServer() override;
+#pragma endregion
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	TObjectPtr<UTSInventoryMasterComponent> InventoryComp;
+};
