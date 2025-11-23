edca5ae768d0d09ca16f7235679ea99c728b7a56
seop0102 <seop0102@kookmin.ac.kr>
2025-11-23 22:24:34 +0900

    feat(Character) : LeftClick시 자원원천인지 확인하도록 구현

---
+++ Source/TinySurvivor/Private/GAS/GA/GA_LeftClick.cpp
@@ -0,0 +1,47 @@
+#include "GAS/GA/GA_LeftClick.h"
+
+#include "rdInstances.h"
+#include "Character/TSCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "System/ResourceControl/TSResourceItemInterface.h"
+
+UGA_LeftClick::UGA_LeftClick()
+{
+	
+}
+
+void UGA_LeftClick::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	BoxTrace();
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UGA_LeftClick::BoxTrace()
+{
+	ATSCharacter* Character = Cast<ATSCharacter>(GetAvatarActorFromActorInfo());
+	const FVector Start = Character->GetActorLocation();
+	const FVector End = Start + (Character->GetActorForwardVector() * AttackRange);
+	
+	TArray<FHitResult> Hits; //박스트레이스 안에 들어온 모든 거 배열로 -> 반복문으로 인터페이스 골라내기
+	TArray<AActor*> Ignored;
+	Ignored.Add(Character);
+	
+	bool bHit = UKismetSystemLibrary::BoxTraceMulti(this, Start, End, BoxHalfSize, Character->GetActorRotation(),UEngineTypes::ConvertToTraceType(ECC_Visibility), false, Ignored, EDrawDebugTrace::ForDuration, Hits, true);
+
+	if (bHit)
+	{
+		for (FHitResult Hit : Hits)
+		{
+			AActor* HitActor = Hit.GetActor();
+			ITSResourceItemInterface* ResourceInterface = Cast<ITSResourceItemInterface>(HitActor);
+			if (ResourceInterface) //만약 자원원천이면 
+			{
+				//함수 불러오기
+				ResourceInterface->GetItemFromResource(0, Hit.ImpactPoint, Hit.ImpactNormal, Character->GetActorLocation(), Character->GetActorForwardVector());
+				//ㄴ 주먹이라 Required Tool ID = 0
+				return;
+			}
+		}
+	}
+}
---
+++ Source/TinySurvivor/Public/GAS/GA/GA_LeftClick.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseAbility/TSGameplayAbilityBase.h"
+#include "GA_LeftClick.generated.h"
+
+UCLASS()
+class TINYSURVIVOR_API UGA_LeftClick : public UTSGameplayAbilityBase
+{
+	GENERATED_BODY()
+public: 
+	UGA_LeftClick();
+	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
+	float AttackRange = 100.0;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
+	FVector BoxHalfSize = FVector(30.f, 30.f, 30.f);
+	
+private:
+	void BoxTrace();
+};
