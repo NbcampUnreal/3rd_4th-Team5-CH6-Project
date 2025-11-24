23d094705e0b975ab2f674e521abdda06241cb87
SKim041 <sehee1806@gmail.com>
2025-11-24 21:29:08 +0900

    feat(Item): TSEquippedItem 클래스 추가
    캐릭터가 장착한 아이템을 관리하기 위한 `ATSEquippedItem` 클래스 구현.
    - `UStaticMeshComponent`를 생성하고 기본 충돌 설정 및 복제 속성 활성화
    - `SetMesh` 함수 추가로 메쉬 설정 로직 구현
    - 클라이언트-서버 간 동기화를 위한 `GetLifetimeReplicatedProps` 함수 오버라이드

---
+++ Source/TinySurvivor/Private/Item/TSEquippedItem.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/TSEquippedItem.h"
+
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+ATSEquippedItem::ATSEquippedItem()
+{
+	bReplicates = true;
+	bAlwaysRelevant = true;
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	SetRootComponent(MeshComp);
+	
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	MeshComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	MeshComp->SetIsReplicated(true);
+}
+
+void ATSEquippedItem::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ATSEquippedItem, MeshComp);
+}
+
+void ATSEquippedItem::SetMesh(UStaticMesh* Mesh)
+{
+	if (MeshComp&&Mesh)
+	{
+		MeshComp->SetStaticMesh(Mesh);
+	}
+}
---
+++ Source/TinySurvivor/Public/Item/TSEquippedItem.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TSEquippedItem.generated.h"
+
+UCLASS()
+class TINYSURVIVOR_API ATSEquippedItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATSEquippedItem();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	UStaticMeshComponent* MeshComp;
+	
+	void SetMesh(UStaticMesh* Mesh);
+};
