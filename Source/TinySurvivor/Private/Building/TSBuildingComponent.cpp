b1b640a029cda133dd4738d9ce77b8a9cfdcd381
SKim041 <sehee1806@gmail.com>
2025-12-02 20:30:32 +0900

    feat(Building): 건축 시스템 컴포넌트 기본 구현
    - UTSBuildingComponent 클래스 추가
    - 건축 모드 진입 및 종료 기능 구현
    - 프리뷰 메시 생성, 업데이트, 삭제 로직 추가
    - 설치 가능 여부 검증 및 라인 트레이스 기능 구현
    - 빌딩 액터 생성과 서버-클라이언트 동기화 함수 구현

---
+++ Source/TinySurvivor/Private/Building/TSBuildingComponent.cpp
@@ -0,0 +1,313 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Building/TSBuildingComponent.h"
+
+#include "Engine/World.h"
+#include "CollisionQueryParams.h"
+#include "GameFramework/Pawn.h"
+#include "Item/Data/BuildingData.h"
+#include "Item/System/ItemDataSubsystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+
+// Sets default values for this component's properties
+UTSBuildingComponent::UTSBuildingComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+}
+
+// Called when the game starts
+void UTSBuildingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	// 아이템 데이터 서브시스템 초기화
+	CachedIDS = GetItemDataSubsystem();
+	if (!CachedIDS)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to get ItemDataSubsystem!"));
+	}
+}
+
+
+// Called every frame
+void UTSBuildingComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                         FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (!bIsBuildingMode)
+	{
+		return;
+	}
+	UpdatePreviewMesh(DeltaTime);
+}
+
+void UTSBuildingComponent::StartBuildingMode(int32 BuildingDataID)
+{
+	bIsBuildingMode = true;
+	CurrentBuildingDataID = BuildingDataID;
+	SetComponentTickEnabled(true);
+	CreatePreviewMesh(BuildingDataID);
+}
+
+void UTSBuildingComponent::EndBuildingMode()
+{
+	bIsBuildingMode = false;
+	SetComponentTickEnabled(false);
+	bCanPlace = false;
+	CurrentBuildingDataID = 0;
+	DestroyPreviewMesh();
+}
+
+void UTSBuildingComponent::CreatePreviewMesh(int32 BuildingDataID)
+{
+	// 프리뷰 메시 컴포넌트 제거하고 시작
+	if (PreviewMeshComp)
+	{
+		DestroyPreviewMesh();
+	}
+
+	// 빌딩 데이터 조회
+	FBuildingData BuildingData;
+	if (!GetBuildingData(BuildingDataID, BuildingData))
+	{
+		return;
+	}
+
+	if (BuildingData.WorldMesh.IsNull())
+	{
+		return;
+	}
+
+	// 프리뷰 메시 컴포넌트 생성
+	PreviewMeshComp = NewObject<UStaticMeshComponent>(GetOwner());
+	PreviewMeshComp->RegisterComponent();
+	PreviewMeshComp->AttachToComponent(
+		GetOwner()->GetRootComponent(),
+		FAttachmentTransformRules::KeepRelativeTransform
+	);
+	PreviewMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 메시 로드
+	UStaticMesh* PreviewMesh = BuildingData.WorldMesh.LoadSynchronous();
+	if (PreviewMesh)
+	{
+		PreviewMeshComp->SetStaticMesh(PreviewMesh);
+	}
+}
+
+void UTSBuildingComponent::DestroyPreviewMesh()
+{
+	if (PreviewMeshComp)
+	{
+		PreviewMeshComp->DestroyComponent();
+		PreviewMeshComp = nullptr;
+	}
+}
+
+
+void UTSBuildingComponent::UpdatePreviewMesh(float DeltaTime)
+{
+	FHitResult HitResult = BuildingLineTrace();
+	if (!HitResult.IsValidBlockingHit() || !PreviewMeshComp)
+	{
+		return;
+	}
+	PreviewMeshComp->SetWorldLocation(HitResult.Location);
+	bCanPlace = ValidatePlacement(HitResult);
+	if (bCanPlace)
+	{
+		LastTransform = FTransform(FRotator::ZeroRotator, HitResult.Location);
+	}
+	// 설치 가능 여부로 프리뷰 메시 색상 변경
+	if (PreviewMaterial)
+	{
+		UMaterialInstanceDynamic* DynamicMaterial = PreviewMeshComp->CreateDynamicMaterialInstance(0, PreviewMaterial);
+		if (DynamicMaterial)
+		{
+			DynamicMaterial->SetVectorParameterValue(FName("Color"),
+			                                         bCanPlace ? FLinearColor::Green : FLinearColor::Red);
+		}
+	}
+}
+
+FHitResult UTSBuildingComponent::BuildingLineTrace()
+{
+	APlayerController* PC = Cast<APlayerController>(GetOwner()->GetInstigatorController());
+	if (!IsValid(PC) || !PC->IsLocalController())
+	{
+		return FHitResult();
+	}
+
+	// 화면 중앙 가져오기
+	FVector2D ViewPortSize = FVector2D::ZeroVector;
+	GEngine->GameViewport->GetViewportSize(ViewPortSize);
+	const FVector2D ViewportCenter = ViewPortSize / 2.f;
+
+	FVector TraceStart; // 시작 점 
+	FVector Forward;
+
+	// 2D 스크린 좌표를 3D 월드 좌표로 변환
+	bool DeprojectScreenToWorld = UGameplayStatics::DeprojectScreenToWorld(
+		GetWorld()->GetFirstPlayerController(), ViewportCenter, TraceStart, Forward);
+	if (!DeprojectScreenToWorld)
+	{
+		return FHitResult();
+	}
+
+	FVector TraceEnd = TraceStart + Forward * 500.f;
+
+	// 충돌 무시 설정 (플레이어 자신 무시)
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(GetOwner());
+
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ECC_GameTraceChannel3, QueryParams);
+	return HitResult;
+}
+
+bool UTSBuildingComponent::ValidatePlacement(FHitResult HitResult)
+{
+	if (!HitResult.IsValidBlockingHit() || !PreviewMeshComp)
+	{
+		return false;
+	}
+	// 지면이 아닌 곳에 설치하는지 확인
+	const float DotProduct = FVector::DotProduct(HitResult.Normal, FVector::UpVector);
+	if (DotProduct < 0.7f)
+	{
+		return false;
+	}
+
+	// 충돌 체크
+	FBuildingData BuildingData;
+	FVector CheckExtent = FVector(100.f, 100.f, 100.f);
+	if (GetBuildingData(CurrentBuildingDataID, BuildingData))
+	{
+		if (PreviewMeshComp && PreviewMeshComp->GetStaticMesh())
+		{
+			FBoxSphereBounds Bounds = PreviewMeshComp->GetStaticMesh()->GetBounds();
+			CheckExtent = Bounds.BoxExtent * 0.9f;
+		}
+	}
+
+	// 오버랩 체크
+	if (!CheckOverlap(HitResult.Location, CheckExtent))
+	{
+		return false;
+	}
+	return true;
+}
+
+void UTSBuildingComponent::ConfirmPlacement()
+{
+	if (!bIsBuildingMode || !bCanPlace)
+	{
+		return;
+	}
+	ServerSpawnBuilding(CurrentBuildingDataID, LastTransform);
+	EndBuildingMode();
+}
+
+void UTSBuildingComponent::ServerSpawnBuilding_Implementation(int32 BuildingDataID, FTransform SpawnTransform)
+{
+	// 빌딩 액터 스폰
+	FBuildingData BuildingData;
+	if (!GetBuildingData(BuildingDataID, BuildingData))
+	{
+		return;
+	}
+	if (!BuildingData.ActorClass)
+	{
+		return;
+	}
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = GetOwner();
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(
+		BuildingData.ActorClass,
+		SpawnTransform,
+		SpawnParams
+	);
+}
+
+bool UTSBuildingComponent::ServerSpawnBuilding_Validate(int32 BuildingDataID, FTransform SpawnTransform)
+{
+	return BuildingDataID > 0 && SpawnTransform.IsValid();
+}
+
+bool UTSBuildingComponent::GetBuildingData(int32 BuildingDataID, FBuildingData& OutBuildingData)
+{
+	if (BuildingDataID == 0)
+	{
+		return false;
+	}
+	if (!CachedIDS)
+	{
+		CachedIDS = GetItemDataSubsystem();
+	}
+	if (CachedIDS && CachedIDS->GetBuildingDataSafe(BuildingDataID, OutBuildingData))
+	{
+		return true;
+	}
+	return false;
+}
+
+UItemDataSubsystem* UTSBuildingComponent::GetItemDataSubsystem() const
+{
+	if (UGameInstance* GI = GetWorld()->GetGameInstance())
+	{
+		return GI->GetSubsystem<UItemDataSubsystem>();
+	}
+	return nullptr;
+}
+
+bool UTSBuildingComponent::CheckOverlap(const FVector& Location, const FVector& Extent)
+{
+	// 설치 가능 여부 리턴
+
+	// 무시할 액터 설정
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(GetOwner());
+
+	TArray<AActor*> OutActors;
+	// 여러 ObjectType 체크
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_Pawn)); // Pawn
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldDynamic)); // 동적 오브젝트 (구조물)
+	ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldStatic)); // 정적 오브젝트
+
+	bool bHasOverlap = UKismetSystemLibrary::BoxOverlapActors(
+		GetWorld(),
+		Location,
+		Extent,
+		ObjectTypes,
+		nullptr,
+		IgnoreActors,
+		OutActors);
+
+	if (!bHasOverlap)
+	{
+		return true;
+	}
+	for (AActor* OverlappedActor : OutActors)
+	{
+		if (!OverlappedActor)
+		{
+			continue;
+		}
+		// 1. 플레이어 / 몬스터
+		if (OverlappedActor->IsA(APawn::StaticClass()))
+		{
+			return false;
+		}
+
+		// 2. 설치된 구조물 (Building 태그 사용)
+		if (OverlappedActor->ActorHasTag(FName("BlockBuilding")))
+		{
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Source/TinySurvivor/Public/Building/TSBuildingComponent.h
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TSBuildingComponent.generated.h"
+
+
+class UItemDataSubsystem;
+struct FBuildingData;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TINYSURVIVOR_API UTSBuildingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UTSBuildingComponent();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	// 위젯에서 호출
+	UFUNCTION(BlueprintCallable, Category = "Building")
+	void StartBuildingMode(int32 BuildingDataID);
+	UFUNCTION(BlueprintCallable, Category = "Building")
+	void EndBuildingMode();
+	// 빌딩모드 진입 여부 리턴
+	bool IsBuildingMode() { return bIsBuildingMode; }
+	// 좌클릭으로 설치 확정
+	void ConfirmPlacement();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	// 프리뷰 메시 생성 및 삭제
+	void CreatePreviewMesh(int32 BuildingDataID);
+	void DestroyPreviewMesh();
+	// 라인 트레이스 및 위치 업데이트
+	void UpdatePreviewMesh(float DeltaTime);
+	FHitResult BuildingLineTrace();
+	// 설치 가능 여부 검증
+	bool ValidatePlacement(FHitResult HitResult);
+	// 서버에서 액터 스폰
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSpawnBuilding(int32 BuildingDataID, FTransform SpawnTransform);
+	// 빌딩 데이터 조회
+	bool GetBuildingData(int32 BuildingDataID, FBuildingData& OutBuildingData);
+	UItemDataSubsystem* GetItemDataSubsystem() const;
+
+	// 오버랩 체크
+	bool CheckOverlap(const FVector& Location, const FVector& Extent);
+	// 오버랩 체크 시 사용할 충돌 채널
+	UPROPERTY(EditDefaultsOnly, Category = "Building|Settings")
+	TEnumAsByte<ECollisionChannel> OverlapChannel = ECC_Pawn;
+
+	// 빌딩 관련 변수
+	bool bIsBuildingMode = false;
+	bool bCanPlace = false;
+	int32 CurrentBuildingDataID = 0;
+
+	// 프리뷰 메시 관련 변수
+	UPROPERTY()
+	TObjectPtr<UStaticMeshComponent> PreviewMeshComp;
+	UPROPERTY(EditDefaultsOnly)
+	UMaterialInterface* PreviewMaterial;
+	// 마지막 유효한 설치 위치
+	FTransform LastTransform;
+
+	mutable UItemDataSubsystem* CachedIDS = nullptr;
+};
