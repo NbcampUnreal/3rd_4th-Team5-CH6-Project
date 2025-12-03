77b3687ec22cbb93f8a577bf0799b7c46000036a
seop0102 <seop0102@kookmin.ac.kr>
2025-12-03 17:13:31 +0900

    feat(LeftClick) : 무기/도구별 몽타주, 노티파이, 박스 트레이스 기반 공격 시스템 구현
    - 무기 AnimType에 따라 다른 공격 몽타주 재생하도록 확장
    - 공격 속도, 공격력, 공격 범위 등 캐릭터 스탯 기반 계산 적용
    - Montage 노티파이 수신 후 박스트레이스 판정 처리
    - 자원 채집, 적 타격 로직 통합
    - EnemyDamageEffect 추가

--- Source/TinySurvivor/Public/GAS/GA/GA_LeftClick.h
+++ Source/TinySurvivor/Public/GAS/GA/GA_LeftClick.h
@@ -5,6 +5,8 @@
 #include "Item/Data/Common/ItemCommonEnums.h"
 #include "GA_LeftClick.generated.h"
 
+class UGameplayEffect;
+
 UCLASS()
 class TINYSURVIVOR_API UGA_LeftClick : public UTSGameplayAbilityBase
 {
@@ -12,21 +14,41 @@ class TINYSURVIVOR_API UGA_LeftClick : public UTSGameplayAbilityBase
 public: 
 	UGA_LeftClick();
 	
+	//---------------------활성화 (GE 적용) + 몽타주 재생---------------------
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
 	float AttackRange = 100.0;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
 	FVector BoxHalfSize = FVector(30.f, 30.f, 60.f);
 	
+	// -----------------------몽타주-----------------------
+	//기본 공격 (맨손, 일반 아이템)
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
-	TObjectPtr<UAnimMontage> NoneMontage; // 주먹
+	TObjectPtr<UAnimMontage> NoneMontage;
+
+	//도구
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
+	TObjectPtr<UAnimMontage> PickMontage; // 곡괭이 타입 도구
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
-	TObjectPtr<UAnimMontage> PickMontage; // 곡괭이?
+	TObjectPtr<UAnimMontage> AxeMontage; // 도끼 타입 도구
 
+	//무기
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
-	TObjectPtr<UAnimMontage> WeaponSpearMontage; //창?
+	TObjectPtr<UAnimMontage> WeaponMeleeMontage; // 근거리 전투용 무기
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
+	TObjectPtr<UAnimMontage> WeaponSpearMontage; // 창 타입 전투용 무기 ------ 보류
+	
+	// -----------------------GE-----------------------
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
+	TSubclassOf<UGameplayEffect> EnemyDamageEffectClass; // 적에게 데미지 입히는 GE 
+	
+	// -----------------------노티파이 받는 함수-----------------------
+	UFUNCTION()
+	void ReceivedNotify(FGameplayEventData EventData);
 private:
 	void BoxTrace(UAbilitySystemComponent* ASC, EItemAnimType ItemAnimType, int32& ATK);
 };
