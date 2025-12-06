bfbc90af0932b579ea8d496fe406494219dcaae5
SKim041 <sehee1806@gmail.com>
2025-12-06 21:22:05 +0900

    feat(UI): 스탯 데이터를 저장하는 StatDataObject 클래스 추가
    UI 요소에서 사용할 스탯 데이터를 정의하고 관리하기 위한 UObject 클래스 추가.
    - StatName, StatValue, StatUnit 프로퍼티를 포함한 `UStatDataObject` 클래스 생성
    - BlueprintReadWrite 카테고리 설정으로 블루프린트에서 데이터 접근 가능

---
+++ Source/TinySurvivor/Public/UI/StatDataObject.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "StatDataObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class TINYSURVIVOR_API UStatDataObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, Category = "Stat")
+	FText StatName;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Stat")
+	FText StatValue;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Stat")
+	FText StatUnit;
+};
