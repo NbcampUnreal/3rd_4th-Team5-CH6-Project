afb6a8c4199a0bb8fda61b62d39eab117142ae58
SKim041 <sehee1806@gmail.com>
2025-12-09 19:09:46 +0900

    feat(Building): 캠프파이어 액터 추가 및 침식 라이트 소스 로직 개선
    캠프파이어 액터 추가 및 침식 라이트 소스 관련 코드 리팩토링 수행.
    - `TSCampfire` 클래스 추가: 목숨 시간(LifeTime)과 불꽃 파티클(FireEffect) 관리
    - 캠프파이어 소멸 시 이펙트 비활성화 및 액터 제거 처리
    - `ErosionLightSourceSubActor` 클래스에서 사용하지 않는 `PointLightComponent` 및 관련 코드 제거
    - 침식도 변화에 따른 빛 조절 로직 단순화 및 초기화 순서 정리
    - `AErosionLightSourceSubActor`에서 재사용되지 않은 불필요한 코드 제거
    - `BeginPlay` 메서드 내 타이머 설정 로직 추가
    - 머티리얼 동적 인스턴스의 생성 및 활용 방식 정리

---
+++ Source/TinySurvivor/Private/Building/Actor/TSCampfire.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Building/Actor/TSCampfire.h"
+
+#include "NiagaraComponent.h"
+#include "Item/Data/BuildingData.h"
+
+
+// Sets default values
+ATSCampfire::ATSCampfire()
+{
+	bSetLightScaleByErosion = false;
+	FireEffect = CreateDefaultSubobject<UNiagaraComponent>(TEXT("FireParticle"));
+	FireEffect->SetupAttachment(RootComponent);
+}
+
+void ATSCampfire::InitializeFromBuildingData(const FBuildingData& BuildingInfo, const int32 StaticDataID)
+{
+	Super::InitializeFromBuildingData(BuildingInfo, StaticDataID);
+	if (HasAuthority())
+	{
+		LifeTime = BuildingInfo.MaxMaintenance;
+	}
+}
+
+// Called when the game starts or when spawned
+void ATSCampfire::BeginPlay()
+{
+	Super::BeginPlay();
+	// LifeTime 지나면 Destory
+	GetWorldTimerManager().SetTimer(LifeTimeTimerHandle, this, &ATSCampfire::OnLifeTimeExpired, LifeTime, false);
+}
+
+void ATSCampfire::OnLifeTimeExpired()
+{
+	if (FireEffect)
+	{
+		FireEffect->Deactivate();
+	}
+	Destroy();
+}
+
