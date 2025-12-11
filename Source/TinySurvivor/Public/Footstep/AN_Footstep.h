71b0bee9889149d4c5f37c2aea7c518c3579f4ed
SKim041 <sehee1806@gmail.com>
2025-12-11 18:38:40 +0900

    feat(Footstep): 발소리 시스템 초기 구현
    캐릭터의 발소리 재생을 위한 컴포넌트 및 애님 노티파이 추가.
    - `UFootstepComponent` 생성: 표면 타입에 따른 발소리 재생 처리
    - `UAN_Footstep` 생성: 애니메이션 노티파이에서 발소리 호출
    - 발소리 재생 시 라인 트레이스로 표면 타입 확인
    - `FootstepSounds` 맵 추가: 표면 타입별 발소리 데이터 관리

---
+++ Source/TinySurvivor/Public/Footstep/AN_Footstep.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_Footstep.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYSURVIVOR_API UAN_Footstep : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "Footstep")
+	FName SocketName = TEXT("foot_l");
+	UPROPERTY(EditAnywhere, Category = "Footstep")
+	bool IsLeft = true;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
