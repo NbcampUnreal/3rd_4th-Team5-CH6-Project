ee7950b9d8c3838272b0b82d3cd399672ca995c1
SKim041 <sehee1806@gmail.com>
2025-12-16 17:19:42 +0900

    feat(Building): 사다리 액터 및 관련 블루프린트 추가
    게임 내 등반 기능을 위한 사다리 액터 및 블루프린트 구현.
    - ATSLadder 클래스 추가: 물리 설정 및 등반 태그 적용
    - BP_Ladder 블루프린트 파일 추가

---
+++ Source/TinySurvivor/Private/Building/Actor/TSLadder.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Building/Actor/TSLadder.h"
+
+
+// Sets default values
+ATSLadder::ATSLadder()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	
+	MeshComponent->SetSimulatePhysics(true);
+	MeshComponent->SetCollisionProfileName("PhysicsActor");
+	// 무게 설정
+	MeshComponent->SetMassOverrideInKg(NAME_None, 5000.0f, true);
+	// 감쇠 높이기 - 움직임이 빨리 멈춤
+	MeshComponent->SetLinearDamping(5.0f); // 기본 0.01, 높을수록 빨리 정지
+	MeshComponent->SetAngularDamping(5.0f); 
+	Tags.Add("Climbable");
+}
+
