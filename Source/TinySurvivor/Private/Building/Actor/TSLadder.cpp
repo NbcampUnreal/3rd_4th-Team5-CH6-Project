5706fdfe7ab0210dc4405fb147ed6c62170eca43
SKim041 <sehee1806@gmail.com>
2025-12-17 22:02:54 +0900

    refactor(Building): 사다리 물리 설정 및 데이터 테이블 수정
    사다리의 물리 속성 관리 로직 개선 및 관련 데이터 테이블 업데이트.
    - TSLadder: 물리 및 무게 설정 로직 수정
    - DT_Building: 폐품 조명 메시 업데이트

--- Source/TinySurvivor/Private/Building/Actor/TSLadder.cpp
+++ Source/TinySurvivor/Private/Building/Actor/TSLadder.cpp
@@ -9,14 +9,15 @@ ATSLadder::ATSLadder()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-	
+	// 물리 설정
 	MeshComponent->SetSimulatePhysics(true);
 	MeshComponent->SetCollisionProfileName("PhysicsActor");
 	// 무게 설정
-	MeshComponent->SetMassOverrideInKg(NAME_None, 5000.0f, true);
+	MeshComponent->BodyInstance.bOverrideMass = true; 
+	MeshComponent->BodyInstance.SetMassOverride(5000.f);
 	// 감쇠 높이기 - 움직임이 빨리 멈춤
 	MeshComponent->SetLinearDamping(5.0f); // 기본 0.01, 높을수록 빨리 정지
-	MeshComponent->SetAngularDamping(5.0f); 
+	MeshComponent->SetAngularDamping(5.0f);
 	Tags.Add("Climbable");
 }
 
