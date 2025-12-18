fc395bdc5e59fe0a7a07c94715730cd98138b209
SKim041 <sehee1806@gmail.com>
2025-12-18 14:33:09 +0900

    feat(Building): 유지 비용 ID 로직 추가 및 관련 UI 개선
    건물 유지 비용 ID를 처리하기 위한 로직 및 UI 통합.
    - `FBuildingDataProvider`: `GetMaintenanceCostID` 함수 추가
    - `IDisplayDataProvider`: `GetMaintenanceCostID` 함수 기본 구현 추가
    - `LightingStatViewer`: 유지 비용 ID에 따라 유지비 관련 통계 표시 로직 수정
    - 최대 동작 시간 및 소모 간격 통계 로직 업데이트

--- Source/TinySurvivor/Private/UI/StatDisplay/Viewer/Building/LightingStatViewer.cpp
+++ Source/TinySurvivor/Private/UI/StatDisplay/Viewer/Building/LightingStatViewer.cpp
@@ -39,26 +39,32 @@ void ULightingStatViewer::ShowStatList(UListView* ListView, const IDisplayDataPr
 		FText StatUnit = DisplaySystem->GetUnit(StatTag);
 		AddStatToList(ListView, StatName, StatValue, StatUnit);
 	}
-	// // 최대 동작 시간
-	// {
-	// 	StatTag = ItemTags::TAG_Display_Stat_MaxMaintenance;
-	// 	float FloatValue = DataProvider.GetStatValue(StatTag);
-	// 	FText StatName = DisplaySystem->GetDisplayName_KR(StatTag);
-	// 	FText StatValue = FText::AsNumber(FloatValue);
-	// 	FText StatUnit = DisplaySystem->GetUnit(StatTag);
-	// 	AddStatToList(ListView, StatName, StatValue, StatUnit);
-	// }
-	// // 연료 재료/소모 간격
-	// {
-	// 	StatTag = ItemTags::TAG_Display_Stat_MaintenanceInterval;
-	// 	float FloatValue = DataProvider.GetStatValue(StatTag);
-	// 	FText StatName = DisplaySystem->GetDisplayName_KR(StatTag);
-	// 	FText StatValue = FText::Format(FText::FromString("{0} | {1}"),
-	// 	                                DataProvider.GetItemName(StatTag),
-	// 	                                FText::AsNumber(FloatValue));
-	// 	FText StatUnit = DisplaySystem->GetUnit(StatTag);
-	// 	AddStatToList(ListView, StatName, StatValue, StatUnit);
-	// }
+	// 모닥불: 최대 동작 시간
+	{
+		StatTag = ItemTags::TAG_Display_Stat_MaxMaintenance;
+		float FloatValue = DataProvider.GetStatValue(StatTag);
+		if (FloatValue > 0.f)
+		{
+			FText StatName = DisplaySystem->GetDisplayName_KR(StatTag);
+			FText StatValue = FText::AsNumber(FloatValue);
+			FText StatUnit = DisplaySystem->GetUnit(StatTag);
+			AddStatToList(ListView, StatName, StatValue, StatUnit);
+		}
+	}
+	// 연료 재료/소모 간격
+	{
+		if (DataProvider.GetMaintenanceCostID() > 0)
+		{
+			StatTag = ItemTags::TAG_Display_Stat_MaintenanceInterval;
+			float FloatValue = DataProvider.GetStatValue(StatTag);
+			FText StatName = DisplaySystem->GetDisplayName_KR(StatTag);
+			FText StatValue = FText::Format(FText::FromString("{0} | {1}"),
+			                                DataProvider.GetItemName(StatTag),
+			                                FText::AsNumber(FloatValue));
+			FText StatUnit = DisplaySystem->GetUnit(StatTag);
+			AddStatToList(ListView, StatName, StatValue, StatUnit);
+		}
+	}
 	// 내구도
 	{
 		StatTag = ItemTags::TAG_Display_Stat_Durability;
