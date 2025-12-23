eadb90480870e638b6543e64b718a170666728fb
chaenning <codmsrjf@naver.com>
2025-12-23 23:47:28 +0900

    feat (Recipe) : 감정표현 사운드 재생 로직 추가
    - BP_GA_Emote : 인사 몽타주, BGM 3종 추가
    - 몽타주 실행 시 각 몽타주에 맞는 배경 음악 재생
    - 움직일 시 해당 배경 음악 재생 중단

--- Source/TinySurvivor/Private/GAS/GA/GA_Emote.cpp
+++ Source/TinySurvivor/Private/GAS/GA/GA_Emote.cpp
@@ -1,7 +1,9 @@
 #include "GAS/GA/GA_Emote.h"
 #include "Character/TSCharacter.h"
+#include "Components/AudioComponent.h"
 #include "EmoteSystem/TSEmoteTypes.h"
 #include "GameplayTags/AbilityGameplayTags.h"
+#include "Kismet/GameplayStatics.h"
 
 UGA_Emote::UGA_Emote()
 {
@@ -27,26 +29,49 @@ void UGA_Emote::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 		
 	case ETSEmoteType::HELLO:
 		FirstMontage = HelloMontage;
+		EmoteSound = nullptr;
 		break;
 		
 	case ETSEmoteType::DANCE1:
 		FirstMontage = Dance1Montage;
+		EmoteSound = Dance1Sound;
 		break;
 		
 	case ETSEmoteType::DANCE2:
 		FirstMontage = Dance2Montage;
+		EmoteSound = Dance2Sound;
 		break;
 		
 	case ETSEmoteType::DANCE3:
 		FirstMontage = Dance3Montage;
+		EmoteSound = Dance3Sound;
 		break;
 	}
 	if (FirstMontage)
 	{
-		PlayMontage();
+		PlayMontage(); // 애니메이션 몽타주 재생
+		
+		if (EmoteSound) // 각 모션 별 사운드 재생
+		{
+			ActiveEmoteSound = UGameplayStatics::SpawnSoundAttached(
+				EmoteSound,
+				GetAvatarActorFromActorInfo()->GetRootComponent()
+			);
+		}
 	} else
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 	}
 	
+}
+
+void UGA_Emote::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	// 어빌리티가 종료되거나 취소될 때 사운드 중지
+	if (ActiveEmoteSound && ActiveEmoteSound->IsPlaying())
+	{
+		ActiveEmoteSound->Stop();
+	}
+
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
--- Source/TinySurvivor/Public/GAS/GA/GA_Emote.h
+++ Source/TinySurvivor/Public/GAS/GA/GA_Emote.h
@@ -12,7 +12,8 @@ public:
 	UGA_Emote();
 	
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-	
+	virtual void EndAbility(FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
 	TObjectPtr<UAnimMontage> HelloMontage;
@@ -22,4 +23,16 @@ protected:
 	TObjectPtr<UAnimMontage> Dance2Montage;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
 	TObjectPtr<UAnimMontage> Dance3Montage;
+	
+	UPROPERTY()
+	TObjectPtr<UAudioComponent> ActiveEmoteSound;
+	
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	TObjectPtr<USoundBase> EmoteSound;
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	TObjectPtr<USoundBase> Dance1Sound;
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	TObjectPtr<USoundBase> Dance2Sound;
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	TObjectPtr<USoundBase> Dance3Sound;
 };
