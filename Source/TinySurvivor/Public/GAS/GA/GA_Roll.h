473704067ffbf1adade3b9a7e426333139b7f65b
seop0102 <seop0102@kookmin.ac.kr>
2026-01-05 17:26:25 +0900

    rafactor(GA) : Roll 어빌리티 실행 정책 변경 및 주석 삭제

--- Source/TinySurvivor/Public/GAS/GA/GA_Roll.h
+++ Source/TinySurvivor/Public/GAS/GA/GA_Roll.h
@@ -14,13 +14,8 @@ class TINYSURVIVOR_API UGA_Roll : public UTSGameplayAbilityBase
 public:
 	UGA_Roll();
 	
-	// Stamina >= 20 && Thirst > 0 이어야 발동
 	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
-	
-	// 활성화 (GE 적용) + 몽타주 재생
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
-	
-	// 종료 (GE 제거 후 Delay GE 적용)
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
 protected:
@@ -33,24 +28,14 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Montage")
 	TObjectPtr<UAnimMontage> RollRightMontage;
 	
-	// Stamina 관련 GE 들 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Roll")
-	TSubclassOf<UGameplayEffect> RollCostEffectClass; // 바로 -20 스태미나
+	TSubclassOf<UGameplayEffect> RollCostEffectClass; 
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Roll")
-	TSubclassOf<UGameplayEffect> StaminaDelayEffectClass; //EndAbility 후 1초 딜레이
+	TSubclassOf<UGameplayEffect> StaminaDelayEffectClass; 
 	
-	// 롤 상태 관련 태그 
-	UPROPERTY(EditDefaultsOnly, Category = "Roll|Tags")
-	FGameplayTag RollMoveTag; // 몽타주 재생동안 state.move.roll 태그 활성화
-
-	UPROPERTY(EditDefaultsOnly, Category = "Roll|Tags")
-	FGameplayTag StaminaBlockTag; // 몽타주 재생동안 stamina 회복 차단
-	
-	//방향 결정 함수
 	UAnimMontage* DetermineRollMontage() const;
 	
-	// 몽타주 종료시 처리 
 	UFUNCTION()
 	void OnRollMontageFinished();
 };
