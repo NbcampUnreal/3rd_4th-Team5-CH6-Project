e502c482bad30002ad971c8ca73438a44a97ab53
SKim041 <sehee1806@gmail.com>
2026-01-05 12:50:26 +0900

    feat(UI): HUD 알림 시스템 및 게임플레이 태그 추가
    HUD 알림 시스템 구축 및 관련 게임플레이 태그 정의.
    - 알림 전용 네이티브 게임플레이 태그 선언 및 정의 추가 (`NotificationTags` 네임스페이스)
      - 크래프팅, 빌딩, 인벤토리 관련 알림 태그 구현
    - `DT_TagDisplay` 데이터 테이블 갱신
    - `WBP_InGameWidget` 위젯 수정
    - HUD 알림 위젯 (`WBP_HUDNotification`) 추가

---
+++ Source/TinySurvivor/Private/GameplayTags/NofiticationTags.cpp
@@ -0,0 +1,27 @@
+﻿#include "GameplayTags/NofiticationTags.h"
+
+namespace NotificationTags
+{
+#pragma region Crafting
+	//========================================
+	// 크래프팅 알림
+	//========================================
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Crafting_Failed, "Notification.Crafting.Failed");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Crafting_LackingIngredients, "Notification.Crafting.LackingIngredients");
+#pragma endregion
+#pragma region Building
+	//========================================
+	// 빌딩 알림
+	//========================================
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Building_Failed, "Notification.Building.Failed");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Building_LackingIngredients, "Notification.Building.LackingIngredients");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Building_CannotPlace, "Notification.Building.CannotPlace");
+#pragma endregion
+#pragma region Inventory
+	//========================================
+	// 인벤토리 알림
+	//========================================
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Inventory_Failed, "Notification.Inventory.Failed");
+	UE_DEFINE_GAMEPLAY_TAG(TAG_Notification_Inventory_CannotPlace, "Notification.Inventory.CannotPlace");
+#pragma endregion
+}
